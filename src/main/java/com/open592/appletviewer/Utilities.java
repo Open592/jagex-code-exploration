9723837d89bb2cd35a204d4cc9962daa9196be59
Open592 Developer <uss@open592.com>
2022-01-25 01:17:22 -0800

    store reference to base64.encoder
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/main/java/com/open592/appletviewer/Utilities.java
+++ src/main/java/com/open592/appletviewer/Utilities.java
@@ -4,10 +4,9 @@ import java.util.Base64;
 
 // $FF: renamed from: app.s
 final class Utilities {
+   static private final Base64.Encoder encoder = Base64.getEncoder();
    // $FF: renamed from: a (int, byte[]) java.lang.String
    static String byteArrayToBase64String(byte[] byteArray) {
-      Base64.Encoder encoder = Base64.getEncoder();
-
       return encoder.encodeToString(byteArray);
    }
 }
