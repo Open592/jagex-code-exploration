9028f3d95d48358a104a7dce9925b7a9133e3e14
Open592 Developer <uss@open592.com>
2025-09-28 00:12:46 -0700

    natives: Remove annotations from native lib stubs
    Signed-off-by: Open592 Developer <uss@open592.com>

--- src/main/java/com/jagex/client/jagex3/jagmisc/jagmisc.java
+++ src/main/java/com/jagex/client/jagex3/jagmisc/jagmisc.java
@@ -1,25 +1,10 @@
 package com.jagex.client.jagex3.jagmisc;
 
-import org.openrs2.deob.annotation.OriginalArg;
-import org.openrs2.deob.annotation.OriginalClass;
-import org.openrs2.deob.annotation.OriginalMember;
-import org.openrs2.deob.annotation.Pc;
-
-@OriginalClass("client!jagex3/jagmisc/jagmisc")
 public final class jagmisc {
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "nanoTime", descriptor = "()J")
   public static native long nanoTime();
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "ping", descriptor = "(BBBBJ)I")
-  public static int ping(
-      @OriginalArg(0) byte arg0,
-      @OriginalArg(1) byte arg1,
-      @OriginalArg(2) byte arg2,
-      @OriginalArg(3) byte arg3,
-      @OriginalArg(4) long arg4)
-      throws Throwable {
-    @Pc(6)
+  public static int ping(byte arg0, byte arg1, byte arg2, byte arg3, long arg4) throws Throwable {
     int local6 = ping0(arg0, arg1, arg2, arg3, arg4);
     if (local6 < 0) {
       throw new Exception(String.valueOf(local6));
@@ -27,37 +12,19 @@ public final class jagmisc {
     return local6;
   }
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "ping0", descriptor = "(BBBBJ)I")
-  private static native int ping0(
-      @OriginalArg(0) byte arg0,
-      @OriginalArg(1) byte arg1,
-      @OriginalArg(2) byte arg2,
-      @OriginalArg(3) byte arg3,
-      @OriginalArg(4) long arg4);
+  private static native int ping0(byte arg0, byte arg1, byte arg2, byte arg3, long arg4);
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "Quit0", descriptor = "()V")
   private static native void Quit0();
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "init", descriptor = "()Z")
   public static native boolean init();
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "quit", descriptor = "()V")
   public static void quit() {
     Quit0();
   }
 
-  @OriginalMember(
-      owner = "client!jagex3/jagmisc/jagmisc",
-      name = "getAvailablePhysicalMemory",
-      descriptor = "()J")
   public static native long getAvailablePhysicalMemory();
 
-  @OriginalMember(
-      owner = "client!jagex3/jagmisc/jagmisc",
-      name = "getTotalPhysicalMemory",
-      descriptor = "()J")
   public static native long getTotalPhysicalMemory();
 
-  @OriginalMember(owner = "client!jagex3/jagmisc/jagmisc", name = "<init>", descriptor = "()V")
   private jagmisc() {}
 }
